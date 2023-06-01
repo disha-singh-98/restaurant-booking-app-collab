6063242391a1751d598d9bc5151acc5bbb419f02
disha-singh-98 <jacksingh80085@gmail.com>
2023-06-01 09:00:00 +0530

    Initial project setup

---
+++ README.md
@@ -0,0 +1,17 @@
+# Restaurant Booking App
+
+A modern web application developed by Disha Singh.
+
+## Features
+- Responsive design
+- Modern UI/UX
+- Interactive components
+- Cross-browser compatibility
+
+## Author
+**Disha Singh**  
+LNCT Student (2022-2026 Batch)  
+Email: jacksingh80085@gmail.com
+
+## License
+MIT License
---
+++ script.js
@@ -0,0 +1,34 @@
+// Modern JavaScript for Restaurant Booking App
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Restaurant Booking App loaded successfully!');
+    
+    // Smooth scrolling for navigation links
+    const navLinks = document.querySelectorAll('nav a[href^="#"]');
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            
+            if (targetElement) {
+                targetElement.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                });
+            }
+        });
+    });
+    
+    // CTA Button animation
+    const ctaButton = document.querySelector('.cta-button');
+    if (ctaButton) {
+        ctaButton.addEventListener('click', function() {
+            this.style.transform = 'scale(0.95)';
+            setTimeout(() => {
+                this.style.transform = 'scale(1)';
+            }, 150);
+            
+            alert('Welcome to Restaurant Booking App! 🚀');
+        });
+    }
+});
